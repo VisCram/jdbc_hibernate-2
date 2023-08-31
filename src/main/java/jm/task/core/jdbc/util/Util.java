550bcbf8b7d3c567331462f6b14b8abcaeed5325
VisCram <126577181+VisCram@users.noreply.github.com>
2023-08-31 14:53:58 +0500

    Задача выполнена

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,23 @@
 package jm.task.core.jdbc.util;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost:3306/test";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "rootroot";
+
+    public static Connection getConnection() {
+        Connection connection;
+        try {
+            Class.forName(DB_DRIVER);
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return connection;
+    }
 }
+
